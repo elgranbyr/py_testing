d6b3c858a0614b75f3b8dc66dbb2c3224eeb9a72
Elgranbyr <byr0378@gmail.com>
2025-03-24 09:24:46 -0600

    activating path in linux

--- enviroments/venv_lint/linux_setup.sh
+++ enviroments/venv_lint/linux_setup.sh
@@ -5,7 +5,7 @@ set -e
 
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$( dirname ${CURR_DIR} )"
-VENV_DIR="${REPO_ROOT}/venv"
+VENV_DIR="${REPO_ROOT}/../venv"
 
 rm -rf ${VENV_DIR}
 
@@ -13,8 +13,10 @@ if [ ! -d ${VENV_DIR} ]; then
     echo "Creating virtual environment in ${VENV_DIR}"
     python3 -m venv ${VENV_DIR}
 fi
+echo "activating virtual environment in ${VENV_DIR}"
 
 ACTIVATE_PATH="${VENV_DIR}/bin/activate"
+echo "activating virtual environment: source ${ACTIVATE_PATH}"
 
 source ${ACTIVATE_PATH}
 pip install -e ".[dev,lint,test]"
